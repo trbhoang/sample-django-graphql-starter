930a6e72b79e39bbbca99e466a1b3614984df63a
Hoang Tran <hoangtran@Hoangs-MacBook-Pro.local>
2021-05-18 11:28:18 +0700

    User model

--- userbase/apps/user/models.py
+++ userbase/apps/user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
